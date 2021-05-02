99e4c72046f0d6944294a1563359a6a7578d99f0
wholeworld <jahaanrafear@gmail.com>
2021-05-02 00:23:31 -0500

    covid tracker practice

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <select
+    @change="onChange"
+    v-model="selected"
+    class="form-select mt-10 block w-full border p-3 rounded"
+  >
+    <option value="0">Select Country</option>
+    <option :value="country.ID" v-for="country in countries" key="country.ID">
+      {{ country.Country }}
+    </option>
+  </select></template
+>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    }
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected)
+
+      this.$emit("get-country", country)
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text--5xl font-bold mb-3">
+      <i class="fas fa-virus"></i>
+      Covid-19 Tracker
+    </div>
+
+    <p>
+      Api By
+      <a href="https://covid19api.com" target="_blank" class="text-blue-300"
+        >Covid19api.com</a
+      >
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
